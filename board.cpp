08df5c7e5eb820fcf6d50a759bcd36e626dba7d7
lew@mimuw.edu.pl <lew@mimuw.edu.pl>
2006-12-17 20:40:16 +0000

    namespace nbr_cnt, optimizations, asserts, cleanup

--- board.cpp
+++ board.cpp
@@ -40,6 +40,8 @@ const bool player_ac          = false;
 const bool color_ac           = false;
 const bool coord_ac           = false;
 const bool v_ac               = false;
+const bool nbr_cnt_ac         = false;
+
 
 const bool chain_ac           = false;
 const bool board_captured_ac  = false;
@@ -47,6 +49,7 @@ const bool board_hash_ac      = false;
 const bool board_color_at_ac  = false;
 const bool board_nbr_cnt_ac   = false;
 const bool chain_at_ac        = false;
+const bool chain_next_v_ac    = false;
 const bool chains_ac          = false;
 const bool board_ac           = false;
 #endif
@@ -56,6 +59,7 @@ const bool player_ac          = true;
 const bool color_ac           = true;
 const bool coord_ac           = true;
 const bool v_ac               = true;
+const bool nbr_cnt_ac         = true;
 
 const bool chain_ac           = true;
 const bool board_captured_ac  = true;
@@ -63,6 +67,7 @@ const bool board_hash_ac      = true;
 const bool board_color_at_ac  = true;
 const bool board_nbr_cnt_ac   = true;
 const bool chain_at_ac        = true;
+const bool chain_next_v_ac    = true;
 const bool chains_ac          = true;
 const bool board_ac           = true;
 #endif
@@ -420,8 +425,49 @@ public:
 
 };
 
+// namespace nbr_cnt
+
+namespace nbr_cnt {
+
+  typedef uint t;
+
+  uint empty_cnt  (t nbr_cnt) { return nbr_cnt >> 8; }
+  uint player_cnt (t nbr_cnt, player::t pl) { return (nbr_cnt >> (pl*4)) & 0xf; }
+
+  const uint player_cnt_is_4_mask [player::cnt] = { (0x4 << (player::black * 4)), (0x4 << (player::white * 4)) };
+
+  uint player_cnt_is_4 (t nbr_cnt, player::t pl) { return nbr_cnt & player_cnt_is_4_mask[pl]; }
+
+  void check (t nbr_cnt) {
+    unused (nbr_cnt);
+    if (!nbr_cnt_ac) return;
+    assert (empty_cnt (nbr_cnt) <= 4);
+    assert (player_cnt (nbr_cnt, player::black) <= 4);
+    assert (player_cnt (nbr_cnt, player::white) <= 4);
+  }
+
+  t of_desc (uint black_cnt, uint white_cnt, uint empty_cnt) {
+    assertc (nbr_cnt_ac, black_cnt <= 4);
+    assertc (nbr_cnt_ac, white_cnt <= 4);
+    assertc (nbr_cnt_ac, empty_cnt <= 4);
+
+    return 
+      (black_cnt << 0) +
+      (white_cnt << 4) +
+      (empty_cnt << 8);
+  }
+  
+  const t player_inc_tab [player::cnt] = { (1 << (player::black * 4)) - (1 << 8), (1 << (player::white * 4)) - (1 << 8) };
+
+  t player_inc (player::t pl) { return player_inc_tab[pl]; }
+
+  const t edge_inc = 1 + (1 << 4) - (1 << 8);
+
+}
+
 
 // class board_t
+
 
 const static zobrist_t zobrist[1];
 
@@ -429,17 +475,16 @@ class board_t {
   
 public:
 
-  color::t  color_at[v::cnt];
-  chain_t   chain_at[v::cnt];
-  v::t      chain_next_v[v::cnt];
-  uint      nbr_cnt[v::cnt]; // incremental, for fast eye checking
+  color::t    color_at[v::cnt];
+  chain_t     chain_at[v::cnt];
+  v::t        chain_next_v[v::cnt];
+  nbr_cnt::t  nbr_cnt[v::cnt]; // incremental, for fast eye checking
   
-  v::t      captured[max_captured_cnt];
-  uint      captured_cnt;
+  v::t        captured[max_captured_cnt];
+  uint        captured_cnt;
 
-  hash::t   hash;
-  float     komi;
-
+  hash::t     hash;
+  float       komi;
 
   enum play_ret_t { play_ok, play_suicide, play_ss_suicide };
 
@@ -494,7 +539,7 @@ public:
       expected_nbr_cnt =        // definition of nbr_cnt[v]
         ((nbr_color_cnt[color::black] + nbr_color_cnt[color::edge]) << 0) +
         ((nbr_color_cnt[color::white] + nbr_color_cnt[color::edge]) << 4) +
-        ((nbr_color_cnt[color::empty] << 8));
+        ((nbr_color_cnt[color::empty]                             ) << 8);
     
       assert (nbr_cnt[v] == expected_nbr_cnt);
     }
@@ -516,6 +561,15 @@ public:
           assert (chain_at[v].find_root_npc () == chain_at[v::E (v)].find_root_npc ());
 
       }
+    }
+  }
+
+  void check_chain_next_v () const {
+    if (!chain_next_v_ac) return;
+    v_for_each (v) {
+      v::check (chain_next_v[v]);
+      if (!color::is_player (color_at[v])) 
+        assert (chain_next_v[v] == v);
     }
   }
 
@@ -541,7 +595,6 @@ public:
       uint forward_edge_cnt;
       uint backward_edge_cnt;
 
-      
       chain_root[act_chain_no] = chain_at[v].find_root_npc ();
       act_root = chain_root[act_chain_no];
       
@@ -574,7 +627,6 @@ public:
 
         act_v = chain_next_v[act_v];
       } while (act_v != v);
-
       
       assert (forward_edge_cnt == backward_edge_cnt);
       assert (act_root->lib_cnt () == lib_cnt);
@@ -588,15 +640,16 @@ public:
   void check () const {
     if (!board_ac) return;
 
-    check_captured ();
-    check_hash ();
-    check_color_at ();
-    check_nbr_cnt ();
-    check_chain_at ();
-    check_chains ();
+    check_captured      ();
+    check_hash          ();
+    check_color_at      ();
+    check_nbr_cnt       ();
+    check_chain_at      ();
+    check_chain_next_v  ();
+    check_chains        ();
   }
 
-  void check_no_more_legal (player::t player) { // for assertion at the end of the playout
+  void check_no_more_legal (player::t player) { // at the end of the playout
     unused (player);
 
     if (!board_ac) return;
@@ -609,6 +662,7 @@ public:
 
   public:
 
+
   board_t () { clear (); }
   
   board_t (const board_t* to_copy) { load (to_copy); }
@@ -620,17 +674,19 @@ public:
 
     rep (v, v::cnt) {
       color_at[v] = color::edge;
-      nbr_cnt[v] = 4 << 8;
+      nbr_cnt[v] = nbr_cnt::of_desc (0, 0, 4);
 
       r = v::row (v);
       c = v::col (v);
 
       if (v::is_on_board (v)) color_at[v] = color::empty;
 
-      if (r == 0 || r == board_size-1) nbr_cnt[v] += 1 + (1 << 4) - (1 << 8);
-      if (c == 0 || c == board_size-1) nbr_cnt[v] += 1 + (1 << 4) - (1 << 8);
+      chain_next_v[v] = v;
+
+      if (r == 0 || r == board_size-1) nbr_cnt[v] += nbr_cnt::edge_inc;
+      if (c == 0 || c == board_size-1) nbr_cnt[v] += nbr_cnt::edge_inc;
       
-      (chain_at+v)->init (nbr_cnt[v] >> 8);
+      (chain_at+v)->init (nbr_cnt::empty_cnt (nbr_cnt[v])); //WW
     }
 
     //captured_top = captured;
@@ -687,26 +743,23 @@ public:
 
     color = color::t (player);
 
-    if (((nbr_cnt[v] >> (4-player*4)) & 0xf) == 4)
+    if (nbr_cnt::player_cnt_is_4 (nbr_cnt[v], player::other (player)))
       return play_no_lib (player, v);
 
-    color_at[v] = color;
-    hash ^= zobrist->of_pl_v (player, v);
+    place_stone (player, v);
     
-    (chain_at+v)->init (nbr_cnt[v] >> 8);
     new_chain_root = &chain_at[v];
-    chain_next_v[v] = v;
 
-    process_new_nbr (v::N(v), color, v, &new_chain_root);
-    process_new_nbr (v::W(v), color, v, &new_chain_root);
-    process_new_nbr (v::E(v), color, v, &new_chain_root);
-    process_new_nbr (v::S(v), color, v, &new_chain_root);
+    process_new_nbr (v::N(v), player, v, &new_chain_root);
+    process_new_nbr (v::W(v), player, v, &new_chain_root);
+    process_new_nbr (v::E(v), player, v, &new_chain_root);
+    process_new_nbr (v::S(v), player, v, &new_chain_root);
 
     assertc (board_ac, new_chain_root == chain_at[v].find_root ());
              
     if (new_chain_root->lib_cnt_is_zero ()) {
       remove_chain(v);
-      assertc (board_ac, chain_next_v[v] != v);
+      assertc (board_ac, captured_cnt > 1);
       return play_suicide;
     }
 
@@ -746,17 +799,12 @@ public:
       return play_ss_suicide;
     }
 
-    color_at[v] = color::t (player);
-
-    hash ^= zobrist->of_pl_v (player, v);
+    place_stone (player, v);
     
-    (chain_at+v)->init (nbr_cnt[v] >> 8);
-    chain_next_v[v] = v;
-
-    nbr_cnt[v::N(v)] += (1 << (player * 4)) - (1 << 8); // TODO macro or function
-    nbr_cnt[v::W(v)] += (1 << (player * 4)) - (1 << 8); 
-    nbr_cnt[v::E(v)] += (1 << (player * 4)) - (1 << 8);
-    nbr_cnt[v::S(v)] += (1 << (player * 4)) - (1 << 8);
+    nbr_cnt[v::N(v)] += nbr_cnt::player_inc (player);
+    nbr_cnt[v::W(v)] += nbr_cnt::player_inc (player);
+    nbr_cnt[v::E(v)] += nbr_cnt::player_inc (player);
+    nbr_cnt[v::S(v)] += nbr_cnt::player_inc (player);
 
     if (chain_root_N->lib_cnt_is_zero ()) remove_chain(v::N(v));
     if (chain_root_W->lib_cnt_is_zero ()) remove_chain(v::W(v));
@@ -768,16 +816,17 @@ public:
     return play_ok;
   }
 
-  void process_new_nbr(v::t v, color::t new_nbr_color, v::t new_nbr_v, chain_t** new_nbr_chain_root) {
+  void process_new_nbr(v::t v, player::t new_nbr_player, v::t new_nbr_v, chain_t** new_nbr_chain_root) {
     chain_t* chain_root;
     
-    nbr_cnt[v] += (1 << (new_nbr_color * 4)) - (1 << 8);
+    nbr_cnt[v] += nbr_cnt::player_inc (new_nbr_player);
+
 
     if (color_at[v] > color::white) return; // not player
 
     chain_root = (chain_at + v)->find_root ();
     
-    if (color_at[v] == new_nbr_color) { // same color of groups
+    if (color_at[v] == color::t (new_nbr_player)) { // same color of groups
       chain_root->dec_lib_cnt ();
       if (chain_root != *new_nbr_chain_root) { // not merged yet
         *new_nbr_chain_root = chain_root->join (*new_nbr_chain_root);
@@ -791,21 +840,16 @@ public:
   
   void remove_chain (v::t v) {
     v::t act_v;
+    v::t tmp_v;
     color::t old_color;
 
     old_color = color_at[v];
     act_v = v;
 
+    assertc (board_ac, color::is_player (old_color));
+
     do {
-      assertc (board_ac, is_player (old_color));
-
-      hash ^= zobrist->of_pl_v (player::t(color_at[act_v]), act_v);
-      color_at[act_v] = color::empty;
-      (chain_at+act_v)->init (nbr_cnt[act_v] >> 8);
-
-      captured[captured_cnt++] = act_v;
-      assertc (board_ac, captured_cnt < v::cnt);
-
+      remove_stone (act_v);
       act_v = chain_next_v[act_v];
     } while (act_v != v);
 
@@ -813,12 +857,32 @@ public:
 
     do {
       v_for_each_nbr (act_v, nbr_v, {
-        nbr_cnt[nbr_v] -= (1 << (old_color * 4)) - (1 << 8);
+        nbr_cnt[nbr_v] -= nbr_cnt::player_inc (player::t (old_color));
         (chain_at+nbr_v)->find_root ()->inc_lib_cnt (); 
       });
 
+      tmp_v = act_v;
       act_v = chain_next_v[act_v];
+      chain_next_v[tmp_v] = tmp_v;
+      
     } while (act_v != v);
+  }
+
+  void place_stone (player::t pl, v::t v) {
+    hash ^= zobrist->of_pl_v (pl, v);
+    color_at[v] = color::t (pl);
+    (chain_at+v)->init (nbr_cnt::empty_cnt (nbr_cnt[v]));
+
+    assertc (chain_next_v_ac, chain_next_v[v] == v);
+  }
+
+  void remove_stone (v::t v) {
+    hash ^= zobrist->of_pl_v (player::t(color_at[v]), v);
+    color_at[v] = color::empty;
+    (chain_at+v)->init (nbr_cnt::empty_cnt (nbr_cnt[v])); // TODO is it needed ?
+    
+    captured[captured_cnt++] = v;
+    assertc (board_ac, captured_cnt < v::cnt);
   }
 
   // utils
@@ -826,7 +890,7 @@ public:
   bool is_eyelike (player::t player, v::t v) { 
     int diag_color_cnt[color::cnt];
 
-    if (((nbr_cnt[v] >> (player * 4)) & 0xf) != 4) return false; // TODO insert macro / function here
+    if (! nbr_cnt::player_cnt_is_4 (nbr_cnt[v], player)) return false;
 
     rep (c, color::cnt) diag_color_cnt[c] = 0;
     diag_color_cnt[color_at[v::NW (v)]]++;
@@ -846,9 +910,8 @@ public:
     rep (v, v::cnt) {       // TODO
       color_score[color_at[v]]++;
       if (color_at[v] == color::empty) {
-        if ((nbr_cnt[v] & 0xf) == 0x4) color_score[color::black]++; // TODO macro
-        else if ((nbr_cnt[v] & 0xf0) == 0x40) color_score[color::white]++; 
-        // TODO test it (removin else may speed it up)
+             if (nbr_cnt::player_cnt_is_4 (nbr_cnt[v], player::black)) color_score[color::black]++;
+        else if (nbr_cnt::player_cnt_is_4 (nbr_cnt[v], player::white)) color_score[color::white]++;
       }
     }
     return color_score[color::black] - color_score[color::white];
