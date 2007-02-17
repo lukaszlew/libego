837766e85749f4b042f0b8bae700e6fd7db0f34c
Łukasz Lew <lew@mimuw.edu.pl>
2007-02-17 03:27:09 +0100

    name change: EGO library

--- main.cpp
+++ main.cpp
@@ -1,25 +1,26 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
  *                                                                           *
- *  This file is part of GoBoard library,                                    *
+ *  This file is part of Library of Effective GO routines - EGO library      *
  *                                                                           *
  *  Copyright 2006, 2007 Lukasz Lew                                          *
  *                                                                           *
- *  GoBoard library is free software; you can redistribute it and/or modify  *
+ *  EGO library is free software; you can redistribute it and/or modify      *
  *  it under the terms of the GNU General Public License as published by     *
  *  the Free Software Foundation; either version 2 of the License, or        *
  *  (at your option) any later version.                                      *
  *                                                                           *
- *  GoBoard library is distributed in the hope that it will be useful,       *
+ *  EGO library is distributed in the hope that it will be useful,           *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
  *  GNU General Public License for more details.                             *
  *                                                                           *
  *  You should have received a copy of the GNU General Public License        *
- *  along with GoBoard library; if not, write to the Free Software           *
+ *  along with EGO library; if not, write to the Free Software               *
  *  Foundation, Inc., 51 Franklin St, Fifth Floor,                           *
  *  Boston, MA  02110-1301  USA                                              *
  *                                                                           *
 \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
