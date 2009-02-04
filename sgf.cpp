8702a765581b5f2a8a02f05ab699f77b7666ef8a
Lukasz Lew <lukasz.lew@gmail.com>
2009-02-04 02:41:30 +0100

    sgf split into header (.h) and (.cpp)

--- sgf.cpp
+++ sgf.cpp
@@ -21,263 +21,249 @@
  *                                                                           *
 \* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+#include "sgf.h"
 
 const string embedded_gtp_tag = "@gtp";
 
 
-class SgfNodeProperties {
-public:
-  map <string, list <string> > property_map;
-
-
-  void add (string name, string value) {
-    // TODO check existance
-    property_map [name] . push_back (value);
-  }
+void SgfNodeProperties::add (string name, string value) {
+  // TODO check existance
+  property_map [name] . push_back (value);
+}
   
 
-  string to_sgf_string () {
-    ostringstream out;
-    for_each (prop, property_map) {
-      if (prop->second.size () > 0) {
-        out << prop->first; // prop_name
-        for_each (prop_value, prop->second) {
-          out << "[" << *prop_value << "]";
-        }
+string SgfNodeProperties::to_sgf_string () {
+  ostringstream out;
+  for_each (prop, property_map) {
+    if (prop->second.size () > 0) {
+      out << prop->first; // prop_name
+      for_each (prop_value, prop->second) {
+        out << "[" << *prop_value << "]";
       }
     }
-    return out.str ();
   }
+  return out.str ();
+}
 
 
-  string get_comment () { 
-    ostringstream out;
-    list <string> comments = property_map ["C"];
-    for_each (comment, comments) {
-      out << *comment << endl;
-    }
-    return out.str ();
+string SgfNodeProperties::get_comment () { 
+  ostringstream out;
+  list <string> comments = property_map ["C"];
+  for_each (comment, comments) {
+    out << *comment << endl;
   }
+  return out.str ();
+}
 
-  void set_comment (string comment) { 
-    property_map ["C"].clear ();
-    add ("C", comment);
+void SgfNodeProperties::set_comment (string comment) { 
+  property_map ["C"].clear ();
+  add ("C", comment);
+}
+
+string SgfNodeProperties::get_embedded_gtp () { 
+  string comment = get_comment (); // TODO tolower 
+  int gtppos = comment.find (embedded_gtp_tag);
+  if (gtppos == -1) return "";
+  gtppos += embedded_gtp_tag.size ();
+  return comment.substr (gtppos); 
+}
+
+// color to allow add empty
+list <Vertex> SgfNodeProperties::get_vertices_to_play (Color color) { 
+  list <Vertex> ret;
+
+#define add_moves(prop_name)                                    \
+  {                                                             \
+    list <string> prop_values = property_map [prop_name];       \
+    for_each (sgf_coord, prop_values) {                         \
+      ret.push_back (Vertex::of_sgf_coords (*sgf_coord));       \
+    }                                                           \
   }
-
-  string get_embedded_gtp () { 
-    string comment = get_comment (); // TODO tolower 
-    int gtppos = comment.find (embedded_gtp_tag);
-    if (gtppos == -1) return "";
-    gtppos += embedded_gtp_tag.size ();
-    return comment.substr (gtppos); 
-  }
-
-  // color to allow add empty
-  list <Vertex> get_vertices_to_play (Color color) { 
-    list <Vertex> ret;
-
-      #define add_moves(prop_name)                              \
-      {                                                         \
-        list <string> prop_values = property_map [prop_name];   \
-        for_each (sgf_coord, prop_values) {                     \
-          ret.push_back (Vertex::of_sgf_coords (*sgf_coord)); \
-        }                                                       \
-      }
       
-    if (color == Color::empty ()) {
-      add_moves ("AE")
-    } else if (color == Color::black ()) {
-      add_moves ("B");
-      add_moves ("AB");
-    } else if (color == Color::white ()) {
-      add_moves ("W");
-      add_moves ("AW");
-    } else {
-      fatal_error ("get_moves: illegal color");
-    }
-    
-    #undef add_moves
-
-    return ret;
+  if (color == Color::empty ()) {
+    add_moves ("AE")
+      } else if (color == Color::black ()) {
+    add_moves ("B");
+    add_moves ("AB");
+  } else if (color == Color::white ()) {
+    add_moves ("W");
+    add_moves ("AW");
+  } else {
+    fatal_error ("get_moves: illegal color");
   }
 
-  list <Vertex> get_vertices_to_clear () { return get_vertices_to_play (Color::empty ()); }
+  return ret;
 
-  list <Vertex> get_vertices_to_play (Player pl) { 
-    return get_vertices_to_play (Color (pl));
-  }
+#undef add_moves
+}
 
-  string get_single_property (string prop_name, string default_value = "") {
-    if (property_map [prop_name].size () == 0) return default_value;
-    if (property_map [prop_name].size () > 1) {
-      cerr << "warning: multiple property value for: " << prop_name << endl;
-    } 
-    return property_map [prop_name].front ();
-  }
+list <Vertex> SgfNodeProperties::get_vertices_to_clear () {
+  return get_vertices_to_play (Color::empty ());
+}
 
-  uint  get_board_size () { return atoi (get_single_property ("SZ", "19").data ()); }
-  float get_komi       () { return atof (get_single_property ("KM", "0.0").data ()); }
+list <Vertex> SgfNodeProperties::get_vertices_to_play (Player pl) { 
+  return get_vertices_to_play (Color (pl));
+}
 
-};
+string SgfNodeProperties::get_single_property (string prop_name,
+                                               string default_value) {
+  if (property_map [prop_name].size () == 0) return default_value;
+  if (property_map [prop_name].size () > 1) {
+    cerr << "warning: multiple property value for: " << prop_name << endl;
+  } 
+  return property_map [prop_name].front ();
+}
+
+uint  SgfNodeProperties::get_board_size () {
+  return atoi (get_single_property ("SZ", "19").data ());
+}
+
+float SgfNodeProperties::get_komi () {
+  return atof (get_single_property ("KM", "0.0").data ());
+}
 
 
 
 // class SgfNode
 
-class SgfNode {
-public:
-  list <SgfNode>      children;
-  SgfNodeProperties  properties;
+SgfNode* SgfNode::add_child () {
+  children.push_back (SgfNode ());
+  return &(children.back ());
+}
 
 
-  SgfNode* add_child () {
-    children.push_back (SgfNode ());
-    return &(children.back ());
-  }
+string SgfNode::to_sgf_string (bool print_braces) {
+  ostringstream out;
+  if (print_braces) out << "(";
 
+  out << endl << ";" << properties.to_sgf_string ();
 
-  string to_sgf_string (bool print_braces = true) {
-    ostringstream out;
-    if (print_braces) out << "(";
+  bool print_child_braces = children.size () > 1;
+  for_each (child, children) 
+    out << child->to_sgf_string (print_child_braces);
 
-    out << endl << ";" << properties.to_sgf_string ();
+  if (print_braces) out << ")";
 
-    bool print_child_braces = children.size () > 1;
-    for_each (child, children) 
-      out << child->to_sgf_string (print_child_braces);
-
-    if (print_braces) out << ")";
-
-    return out.str ();
-  }  
-};
+  return out.str ();
+}  
 
 
 // class SgfTree
 
 
-class SgfTree {
-  SgfNode* root;
-
-public:
-
-  SgfTree () : root (new SgfNode) { }
+SgfTree::SgfTree () : root (new SgfNode) { }
 
   
-  ~SgfTree () {
-    delete root;
-  }
+SgfTree::~SgfTree () {
+  delete root;
+}
 
 
-  bool parse_property_value (istream& in, string& prop_value) {
-    char c;
-    while (in.get (c)) {
-      switch (c) {
-      case '\\':
-        in >> c;
-        if (c == ']')
-          prop_value += ']'; // escaped ']'
-        else {
-          prop_value += '\\'; 
-          prop_value += c;
-        }
-        break;
-      case ']':
-        return true;
-      default:
+bool SgfTree::parse_property_value (istream& in, string& prop_value) {
+  char c;
+  while (in.get (c)) {
+    switch (c) {
+    case '\\':
+      in >> c;
+      if (c == ']')
+        prop_value += ']'; // escaped ']'
+      else {
+        prop_value += '\\'; 
         prop_value += c;
-        break;
       }
+      break;
+    case ']':
+      return true;
+    default:
+      prop_value += c;
+      break;
     }
-    return false;
   }
+  return false;
+}
   
-  // constructor
-  bool parse_sgf (istream& in) {
-    SgfNode* new_root = new SgfNode;
-    #define return_fail { delete new_root; return false; }
+// constructor
+bool SgfTree::parse_sgf (istream& in) {
+  SgfNode* new_root = new SgfNode;
+#define return_fail { delete new_root; return false; }
 
-    SgfNode*         current = new_root;
-    stack <SgfNode*> cb_stack; // come-back-here-later-nodes
+  SgfNode*         current = new_root;
+  stack <SgfNode*> cb_stack; // come-back-here-later-nodes
 
-    string              buffer;
-    string              prop_name;
-    string              prop_value;
-    bool                prop_name_already_used = true;
+  string              buffer;
+  string              prop_name;
+  string              prop_value;
+  bool                prop_name_already_used = true;
 
-    char c;
-    while (in >> c) {
-      switch (c) {
+  char c;
+  while (in >> c) {
+    switch (c) {
 
-      case '(': 
-        cb_stack.push (current);
-        break;
+    case '(': 
+      cb_stack.push (current);
+      break;
 
-      case ')': // go back to open_bracket_node
-        current = cb_stack.top ();
-        if (cb_stack.empty ()) return_fail;
-        cb_stack.pop ();
-        break;
+    case ')': // go back to open_bracket_node
+      current = cb_stack.top ();
+      if (cb_stack.empty ()) return_fail;
+      cb_stack.pop ();
+      break;
 
-      case ';': // create new node and attach it to the current one
-        current = current->add_child ();
-        break;
+    case ';': // create new node and attach it to the current one
+      current = current->add_child ();
+      break;
 
       // parsing property
-      case '[':
-        // parsing the property value
-        prop_value = "";
-        if (parse_property_value (in, prop_value) == false) return_fail;
-        current->properties.add (prop_name, prop_value); // we reuse last property, this may be nonlocal in nonvalid sgf files
-        prop_name_already_used = true;
-        break;
+    case '[':
+      // parsing the property value
+      prop_value = "";
+      if (parse_property_value (in, prop_value) == false) return_fail;
+      current->properties.add (prop_name, prop_value); // we reuse last property, this may be nonlocal in nonvalid sgf files
+      prop_name_already_used = true;
+      break;
 
-      case ' ':
-      case '\n':
-      case '\t':
-      case '\r':
-        break;
+    case ' ':
+    case '\n':
+    case '\t':
+    case '\r':
+      break;
 
-      default:
-        // parsing property name
-        if (prop_name_already_used) {
-          prop_name = "";
-          prop_name_already_used = false;
-        }
-        prop_name += c;
-        break;
+    default:
+      // parsing property name
+      if (prop_name_already_used) {
+        prop_name = "";
+        prop_name_already_used = false;
       }
+      prop_name += c;
+      break;
     }
+  }
     
-    if (cb_stack.empty () == false)
-      return_fail;
+  if (cb_stack.empty () == false)
+    return_fail;
 
-    delete root;
-    root = new_root;
-    return true;
-  }
+  delete root;
+  root = new_root;
+  return true;
+}
 
 
-  string to_sgf_string () {
-    ostringstream out;
-    for_each (it, root->children) 
-      out << it->to_sgf_string () << endl; 
-    return out.str ();
-  }
+string SgfTree::to_sgf_string () {
+  ostringstream out;
+  for_each (it, root->children) 
+    out << it->to_sgf_string () << endl; 
+  return out.str ();
+}
   
 
-  bool is_loaded () { return root->children.size () > 0; }
+bool SgfTree::is_loaded () { return root->children.size () > 0; }
 
 
-  SgfNode* game_node () {
-    assert (is_loaded ());
-    return & (root->children.front ());
-  }
+SgfNode* SgfTree::game_node () {
+  assert (is_loaded ());
+  return & (root->children.front ());
+}
 
-  SgfNodeProperties& properties () {
-    return game_node ()->properties;
-  }
-
-};
-
-
+SgfNodeProperties& SgfTree::properties () {
+  return game_node ()->properties;
+}
---
+++ sgf.h
@@ -0,0 +1,55 @@
+#ifndef _SGF_H_
+#define _SGF_H_
+
+extern const string embedded_gtp_tag;;
+
+class SgfNodeProperties {
+public:
+  map <string, list <string> > property_map;
+
+  void add (string name, string value);
+  string to_sgf_string ();
+  string get_comment ();
+  void set_comment (string comment);
+  string get_embedded_gtp ();
+  list <Vertex> get_vertices_to_play (Color color);
+  list <Vertex> get_vertices_to_clear ();
+
+  list <Vertex> get_vertices_to_play (Player pl);
+  string get_single_property (string prop_name, string default_value = "");
+  uint  get_board_size ();
+  float get_komi ();
+};
+
+
+class SgfNode {
+public:
+  list <SgfNode>      children;
+  SgfNodeProperties  properties;
+
+  SgfNode* add_child ();
+  string to_sgf_string (bool print_braces = true);
+};
+
+
+class SgfTree {
+  SgfNode* root;
+
+public:
+
+  SgfTree ();
+  ~SgfTree ();
+  bool parse_property_value (istream& in, string& prop_value);
+
+  // constructor
+  bool parse_sgf (istream& in);
+  string to_sgf_string ();
+  bool is_loaded ();
+  SgfNode* game_node ();
+  SgfNodeProperties& properties ();
+};
+
+
+
+
+#endif
