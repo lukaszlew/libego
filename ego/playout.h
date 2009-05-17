468037487558f71057d4b32fca814f66b8bf8be9
Lukasz Lew <lukasz.lew@gmail.com>
2009-05-17 16:08:21 +0200

    little playout simplification

--- ego/playout.h
+++ ego/playout.h
@@ -12,7 +12,7 @@ const bool use_mercy_rule      = false;
 const uint mercy_threshold     = 25;
 const uint max_playout_length  = board_area * 2;
 
-enum playout_status_t { pass_pass, mercy, too_long };
+enum PlayoutStatus { pass_pass, mercy, too_long};
 
 template <typename Policy> class Playout {
 public:
@@ -20,33 +20,41 @@ public:
   Board*   board;
   Move*    move_history;
   uint     move_history_length;
+  PlayoutStatus status;
 
   Playout (Policy* policy_, Board*  board_) : policy (policy_), board (board_) {}
 
+  all_inline flatten
+  bool playout_end() {
+    if (board->both_player_pass ()) {
+      status = pass_pass;
+      return true;
+    }
+
+    if (board->move_no >= max_playout_length) {
+      status = too_long;
+      return true;
+    }
+
+    if (use_mercy_rule &&
+        uint (abs (float(board->approx_score ()))) > mercy_threshold) {
+      status = mercy;
+      return true;
+    }
+    return false;
+  }
+
   all_inline
-  playout_status_t run () {
+  PlayoutStatus run () {
     uint begin_move_no = board->move_no;
     move_history = board->move_history + board->move_no;
-    
-    while (true) {
-      if (board->both_player_pass ()) {
-        move_history_length = board->move_no - begin_move_no;
-        return pass_pass;
-      }
-      
-      if (board->move_no >= max_playout_length) {
-        move_history_length = board->move_no - begin_move_no;
-        return too_long;
-      }
-      
-      if (use_mercy_rule &&
-          uint (abs (float(board->approx_score ()))) > mercy_threshold) {
-        move_history_length = board->move_no - begin_move_no;
-        return mercy;
-      }
 
+    while (!playout_end()) {
       policy->play_move (board);
     }
+
+    move_history_length = board->move_no - begin_move_no;
+    return status;
   }
   
 };
