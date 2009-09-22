10a84a3fb6890282f3b86732c4914e18a16f960a
Lukasz Lew <lukasz.lew@gmail.com>
2009-09-22 13:09:05 +0200

    Introduced Allocator in template parameters of FastTree.

--- example/fast_tree.h
+++ example/fast_tree.h
@@ -3,12 +3,13 @@
 
 // -----------------------------------------------------------------------------
 
-template <class Data>
+// TODO replace default std::allocator with FastPool
+template <class Data, template <class N> class TAllocator = std::allocator>
 class Node : public Data {
-public:
+private:
+  typedef TAllocator<Node> Allocator;
 
-  class Iterator;
-  class ChildrenIterator;
+public:
 
   // TODO replace this by placement new in pool or Boost::pool
   Node () {
@@ -32,85 +33,85 @@ public:
     return child_count > 0;
   }
 
+  // ------------------------------------------------------------------
+
+  class ChildrenIterator {
+  public:
+
+  ChildrenIterator(Node& parent) : parent_(parent), act_v_(0) { 
+      Sync ();
+    }
+
+    Node* operator-> () { return parent_.children[act_v_]; }
+    operator Node* ()   { return parent_.children[act_v_]; }
+
+    void operator++ () {
+      act_v_.next();
+      Sync ();
+    }
+
+    operator bool () { return act_v_.in_range(); }
+
+  private:
+    void Sync () {
+      while (act_v_.in_range () && parent_.children[act_v_] == NULL) {
+        act_v_.next();
+      }
+    }
+
+  private:
+    Node& parent_;
+    Vertex act_v_;
+  };
+
+  // ------------------------------------------------------------------
+
+  class Iterator {
+  public:
+    void SetToRoot (Node* root) {
+      path.clear();
+      path.push_back(root);
+    }
+  
+    void ResetToRoot () {
+      assertc (tree_ac, path.size() > 0);
+      path.resize(1);
+    }
+
+    void Descend (Vertex v) {
+      assertc (tree_ac, path.size() > 0);
+      path.push_back(path.back()->children[v]);
+      assertc (tree_ac, ActNode () != NULL);
+    }
+  
+    void Ascend () {
+      assertc (tree_ac, path.size() >= 2);
+      path.pop_back ();
+    }
+
+    vector<Node*>& Path () {
+      assertc (tree_ac, path.size() > 0);
+      return path;
+    }
+
+    Node* operator-> () { return ActNode (); }
+    operator Node* ()   { return ActNode (); }
+
+  private:
+    Node* ActNode() {
+      assertc (tree_ac, path.size() > 0);
+      return path.back();
+    }
+
+  private:
+    vector<Node*> path;
+  };
+
 private:
   FastMap<Vertex, Node*> children;
   int child_count;
 };
 
 // -----------------------------------------------------------------------------
-
-template <class Data>
-class Node<Data> :: Iterator {
-public:
-  void SetToRoot (Node* root) {
-    path.clear();
-    path.push_back(root);
-  }
-  
-  void ResetToRoot () {
-    assertc (tree_ac, path.size() > 0);
-    path.resize(1);
-  }
-
-  void Descend (Vertex v) {
-    assertc (tree_ac, path.size() > 0);
-    path.push_back(path.back()->children[v]);
-    assertc (tree_ac, ActNode () != NULL);
-  }
-  
-  void Ascend () {
-    assertc (tree_ac, path.size() >= 2);
-    path.pop_back ();
-  }
-
-  vector<Node*>& Path () {
-    assertc (tree_ac, path.size() > 0);
-    return path;
-  }
-
-  Node* operator-> () { return ActNode (); }
-  operator Node* ()   { return ActNode (); }
-
-private:
-  Node* ActNode() {
-    assertc (tree_ac, path.size() > 0);
-    return path.back();
-  }
-
-private:
-  vector<Node*> path;
-};
-
-// -----------------------------------------------------------------------------
-
-template <class Data>
-class Node<Data> :: ChildrenIterator {
-public:
-
-  ChildrenIterator(Node& parent) : parent_(parent), act_v_(0) { 
-    Sync ();
-  }
-
-  Node* operator-> () { return parent_.children[act_v_]; }
-  operator Node* ()   { return parent_.children[act_v_]; }
-
-  void operator++ () {
-    act_v_.next();
-    Sync ();
-  }
-
-  operator bool () { return act_v_.in_range(); }
-
-private:
-  void Sync () {
-    while (act_v_.in_range () && parent_.children[act_v_] == NULL) {
-      act_v_.next();
-    }
-  }
-
-private:
-  Node& parent_;
-  Vertex act_v_;
-};
 
 #endif
