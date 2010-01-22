787bbe815ac658f70fca259b0dedf666a5abad20
Lukasz Lew <lukasz.lew@gmail.com>
2010-01-22 17:08:03 +0100

    distributed_twogtp split into worker and admin. Phase 1.

---
+++ source/distributed_twogtp/d2g-worker.cpp
@@ -0,0 +1,39 @@
+#include <QDir>
+#include <QString>
+#include <QtDebug>
+
+#include "database.hpp"
+
+#include "admin.hpp"
+#include "worker.hpp"
+
+void usage (const char* file) {
+  qWarning ()
+    << "Usage: " << file << " [--worker | --admin]";
+}
+
+int main (int argc, char** argv) {
+  srand (time (0));
+
+  if (argc != 2) {
+    usage (argv[0]);
+    return 1;
+  }
+
+  Database db;
+  if (!db.ProcessSettingsFile (QDir::homePath() + "/.my.cnf") || !db.Connect ()) {
+    return 1;
+  }
+
+ if (QString (argv[1]) == "--admin") {
+    Admin admin (db);
+    admin.Run ();
+    return 0;
+  }
+
+  if (QString (argv[1]) == "--worker") {
+    Worker worker (db);
+    worker.Run ();
+    return 0;
+  }
+}
