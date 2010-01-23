a57cbc5b9a52d4cd50f0067a66a16b1e84c5244e
Lukasz Lew <lukasz.lew@gmail.com>
2010-01-23 22:03:17 +0100

    More refactoring.

--- source/engine/mcts_mcmc.hpp
+++ source/engine/mcts_mcmc.hpp
@@ -19,11 +19,6 @@ namespace Param {
   float mcmc_prob_8_nbr = 0.8;
 }
 
-struct McmcNode {
-  NatMap <Move, Stat> move_stats;
-  //  NatMap <Player, Stat> light;
-};
-
 struct Trace {
   Move m1;
   Move m0;
@@ -42,10 +37,9 @@ public:
     // TODO prior randomization here!
     ForEachNat (Move, m1) {
       ForEachNat (Move, m0) {
-        mcmc[m1].move_stats [m0].reset (1.0, 0.0);
+        ms2[m1][m0].reset (1.0, 0.0);
       }
     }
-    //ForEachNat (Player, pl) mcmc[m].light[pl].reset (1.0, 0.0);    
   }
 
   void NewPlayout () {
@@ -57,7 +51,7 @@ public:
     rep (ii, to_update.size() * Param::mcmc_update_fraction) {
       Move m0 = to_update[ii].m0;
       Move m1 = to_update[ii].m1;
-      Stat& s = mcmc [m1].move_stats [m0];
+      Stat& s = ms2 [m1] [m0];
       s.update (score);
       s.UpdateUcb (m0.GetPlayer (), Param::mcmc_explore_coeff);
     }
@@ -72,7 +66,7 @@ public:
 
     Vertex best_v = Vertex::Any(); // any == light move
     float best_value = - 1E20;
-    McmcNode& act_mcmc = mcmc [board.LastMove ()];
+    MS1& my_ms1 = ms2 [board.LastMove ()];
     Player pl = board.ActPlayer();
         
     // TODO to jest za sztywne, 8 sasiadow
@@ -81,7 +75,7 @@ public:
           board.IsLegal (pl, nbr) &&
           !board.IsEyelike (pl, nbr))
       {
-        Stat& stat = act_mcmc.move_stats [Move(pl, nbr)];
+        Stat& stat = my_ms1 [Move(pl, nbr)];
         float value = stat.Ucb(); // TODO UCB?
         if (best_value < value) {
           best_value = value;
@@ -124,29 +118,28 @@ public:
     for_each_8_nbr (pre_v, v, {
       if (board.ColorAt (v) == Color::Empty () && v != pre_move.GetVertex()) {
         Move move = Move (player, v);
-        float mean = mcmc [pre_move].move_stats [move].mean();
+        float mean = ms2 [pre_move] [move].mean();
         stat.update (mean);
       }
     });
 
-//     cerr << "light" << " : "
-//          << mcmc [pre_move].light [player].to_string () << endl;
-
     for_each_8_nbr (pre_v, v, {
-        //ForEachNat (Vertex, v) {
       if (board.ColorAt (v) == Color::Empty () &&
           v != pre_move.GetVertex()) {
         Move move = Move (player, v);
-        float mean = mcmc [pre_move].move_stats [move].mean();
+        float mean = ms2 [pre_move] [move].mean();
         float val = (mean - stat.mean()) / stat.std_dev () / 3;
         gfx->SetInfluence (v.ToGtpString (), val);
         cerr << v.ToGtpString () << " : "
-             << mcmc [pre_move].move_stats [move].to_string () << endl;
+             << ms2 [pre_move] [move].to_string () << endl;
       }
     });
   }
 
-  NatMap <Move, McmcNode> mcmc;
+  typedef NatMap <Move, Stat> MS1;
+  typedef NatMap <Move, MS1> MS2;
+  MS2 ms2;
+
   vector <Trace> to_update;
   uint prob_8mcmc_1024;
 };
