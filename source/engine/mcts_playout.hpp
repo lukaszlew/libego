d09bd2f19cac640bbaaa5e3940d9976af0163236
Lukasz Lew <lukasz.lew@gmail.com>
2010-01-23 14:55:20 +0100

    Local policy.

--- source/engine/mcts_playout.hpp
+++ source/engine/mcts_playout.hpp
@@ -46,29 +46,21 @@ public:
     v_seen.SetToZero();
     all_mcmc.NewPlayout ();
 
-    bool tree_phase = Param::use_mcts_in_playout;
+    tree_phase = Param::use_mcts_in_playout;
 
     // do the playout
     while (true) {
       if (play_board.BothPlayerPass()) break;
       if (move_history.size() >= 3*Board::kArea) return;
       Player pl = play_board.ActPlayer ();
-      Vertex v;
+      Vertex v  = Vertex::Invalid ();
 
-      if (tree_phase) {
-        v = ChooseTreeMove (pl);
-        if (!v.IsValid()) {
-          tree_phase = false;
-          continue;
-        }
-      } else {
-        v = all_mcmc.Choose8Move (play_board, v_seen, random);
-      }
+      //v = all_mcmc.Choose8Move (play_board, v_seen, random);
+      if (!v.IsValid ()) v = ChooseTreeMove (pl);
+      if (!v.IsValid ()) v = ChooseLocalMove ();
+      if (!v.IsValid ()) v = play_board.RandomLightMove (pl, random);
 
-      if (!v.IsValid ()) {
-        v = play_board.RandomLightMove (pl, random);
-      } 
-
+      // TODO simplify this, add tree
       if (play_board.LastVertex().IsValid()) {
         all_mcmc.MovePlayed (play_board.LastMove (), Move (pl, v), v_seen);
       }
@@ -97,8 +89,10 @@ public:
 private:
 
   Vertex ChooseTreeMove (Player pl) {
+    if (!tree_phase) return Vertex::Invalid ();
     if (!ActNode().has_all_legal_children [pl]) {
       if (!ActNode().ReadyToExpand ()) {
+        tree_phase = false;
         return Vertex::Invalid ();
       }
       ASSERT (pl == ActNode().player.Other());
@@ -107,7 +101,29 @@ private:
 
     MctsNode& uct_child = ActNode().BestRaveChild (pl);
     trace.push_back (&uct_child);
+    ASSERT (uct_child.v.IsValid());
     return uct_child.v;
+  }
+  
+  Vertex ChooseLocalMove () {
+    //if (random.GetNextUint (1024) < 128) return Vertex ();
+    Vertex last_v = play_board.LastVertex ();
+    Player pl = play_board.ActPlayer ();
+
+    if (!last_v.IsValid () || last_v == Vertex::Pass ())
+      return Vertex::Invalid ();
+
+    FastStack <Vertex, 8> tab;
+    for_each_8_nbr (last_v, v, {
+      if (play_board.IsLegal(pl, v)) {
+        tab.Push (v);
+      } 
+    });
+    
+    if (tab.Size() <= 0) return Vertex::Invalid ();
+
+    uint i = random.GetNextUint (tab.Size());
+    return tab[i];
   }
 
   void UpdateTrace (int score) {
@@ -165,6 +181,7 @@ private:
   vector <MctsNode*> trace;               // nodes in the path
   vector <Move> move_history;
   NatMap <Vertex, uint> v_seen;
+  bool tree_phase;
 public:
   AllMcmc all_mcmc;
 };
