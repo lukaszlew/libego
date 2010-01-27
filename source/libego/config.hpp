55be2ddadb761c2bbcee3c0c8b41ec3489cc9986
Lukasz Lew <lukasz.lew@gmail.com>
2010-01-27 18:23:31 +0100

    board size fixed

--- source/libego/config.hpp
+++ source/libego/config.hpp
@@ -8,11 +8,6 @@
 // TODO this have to be renamed to max_board_size
 // TODO better use of CMake
 
-#ifdef BOARDSIZE
-const uint board_size = BOARDSIZE;
-#undef BOARDSIZE
-#else
 const uint board_size = 9;
-#endif
 
 #endif
