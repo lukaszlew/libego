86a4a210d6d9de4d3cdd4c2309670ec0c580a634
Lukasz Lew <lukasz.lew@gmail.com>
2010-03-19 21:29:29 +0100

    Parameters implemented in python testing scripts.

--- twogtp/students/client.py
+++ twogtp/students/client.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 
-import xmlrpclib, time, sys, subprocess, os
+import xmlrpclib, time, sys, subprocess, os, re
 
 address = "students.mimuw.edu.pl", 7553
 proxy = xmlrpclib.ServerProxy("http://%s:%d/" % address)
 gnugo = "'gnugo-3.8 --mode gtp --chinese-rules --capture-all-dead --positional-superko --level=0'"
 
 def do_one_series ():
-    log_dir, series_id, game_count, black, white = proxy.get_game_setup ()
+    (log_dir, series_id, game_count, black, white) = proxy.get_game_setup ()
     os.chdir (log_dir)
 
+    sgf_prefix = "game-%d" % series_id
     args = [
         "gogui-twogtp",
         "-auto",
@@ -20,7 +21,7 @@ def do_one_series ():
         "-games", "%d" % game_count,
         "-black", black,
         "-white", white,
-        "-sgffile", "%d" % series_id,
+        "-sgffile", sgf_prefix,
         "-verbose",
         ]
 
@@ -35,9 +36,20 @@ def do_one_series ():
     print "Waiting for results."
     twogtp_log = process.communicate () [0]
 
+    print "LOG:" 
     print twogtp_log
     print
 
+    result_list = []
+    for game_no in range (game_count):
+        file_name = sgf_prefix + "-" + str (game_no) + ".sgf"
+        sgf_file = open (file_name, "r") # TODO r?
+        sgf = sgf_file.read ()
+        sgf_file.close ()
+        winner_letter = re.search (r'RE\[([^]]*)\]', sgf).group(1) [0]
+        assert (black_win in 'BW')
+        result = (game_no % 2, winner_letter == 'B', file_name)
+        result_list.append (result)
     result_list = [(1, 1, "file1.sgf"), (0,0, "file2.sgf")]
     # TODO parse twogtp_log
     proxy.report_game_result (series_id, result_list)
--- twogtp/students/server.py
+++ twogtp/students/server.py
@@ -6,12 +6,20 @@ import random, os, datetime
 
 address = "students.mimuw.edu.pl", 7553
 log_dir = "/home/dokstud/lew/logi/%s" % datetime.datetime.now().isoformat()
+
+
 report_file = "report.csv"
+params_values = {"progressive_bias" : [100.0, 200.0], "explore_coeff" : [0.0, 0.1]}
+params_names  = params_values.keys () #csv header
+
+csv_header = ", ".join (params_names) + ", first_is_black, black_win, sgf_file"
+
 params_of_series = []
 games_per_series = 2
 
 gnugo = "'gnugo-3.8 --mode gtp --chinese-rules --capture-all-dead --positional-superko --level=0'"
 libego = "'/home/dokstud/lew/libego/bin/engine \"LoadGammas /home/dokstud/lew/libego/bin/3x3.gamma\" %s gtp'" 
+gtp_set_command = "set"
 
 # TODO seed, cputime
 
@@ -19,34 +27,45 @@ os.makedirs (log_dir)
 os.chdir (log_dir)
 
 def gen_params ():
-    pass
+    def select_param (x):
+        vals = params_values[x]
+        return x, vals [random.randrange (len (vals))]
+    return dict ((select_param (x) for x in params_values))
+
 
 def params_to_gtp (params):
-    return ""
+    gtp = ""
+    for param_name in params_names:
+        gtp += '"%s %s %s"' % (gtp_set_command, param_name, params[param_name])
+    return gtp
 
-def format_report_header ():
-    return "as_black, black_win, sgf_file"
 
-def format_report_line (series_id, as_black, result, sgf):
-    return "%d, %d, %s" % (as_black, result, sgf)
+def format_report_line (series_id, first_is_black, black_win, sgf):
+    params = params_of_series [series_id]
+    csv_line = ", ".join ((params [param_name] for param_name in params_names))
+    return scv_line + (', %d, %d, "%s"' % (first_is_black, black_win, sgf))
+
 
 def get_game_setup ():
     global series_id
     params = gen_params ()
     params_of_series.append (params)
-    return (log_dir,
-            len (params_of_series),
-            games_per_series,
-            libego % params_to_gtp (params),
-            gnugo
-            )
+    series_id = len (params_of_series)
+    ret = (log_dir,
+           series_id,
+           games_per_series,
+           libego % params_to_gtp (params),
+           gnugo
+           )
+    print len(ret)
+    return ret
 
 
 def report_game_result (series_id, result_list):
     f = open (report_file, "a")
     # TODO check for result length
-    for (as_black, result, sgf) in result_list:
-        f.write ("%s\n" % format_report_line (series_id, as_black, result, sgf))
+    for (first_is_black, black_win, sgf) in result_list:
+        f.write ("%s\n" % format_report_line (series_id, first_is_black, black_win, sgf))
     f.close ()
     return 0
 
@@ -58,7 +77,7 @@ server.register_function (report_game_result, "report_game_result")
 print "Listening on port %d..." % address[1]
 
 f = open (report_file, "a")
-f.write ("%s\n" % format_report_header())
+f.write ("%s\n" % csv_header)
 f.close ()
 
 server.serve_forever()
