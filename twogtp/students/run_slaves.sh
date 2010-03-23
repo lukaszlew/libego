774816565d81e59949ab0c5901b22068fb15eb9d
Lukasz Lew <lukasz.lew@gmail.com>
2010-03-23 02:47:23 +0100

    run_slaves.sh loops over all LAB computers.

--- twogtp/students/run_slaves.sh
+++ twogtp/students/run_slaves.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-# TODO jak ustawic sciezke przez ssh? / wykonac .bashrc
+colors="red pink orange brown yellow khaki cyan green violet blue crimson"
 
-for i in \
-    khaki02 khaki03 khaki04 khaki05 khaki06 khaki07 khaki08 \
-    khaki09 khaki10 khaki11 khaki12 khaki13 khaki14 khaki15 khaki16;
-do
-    for j in 1 2 3 4; do
-        echo -n " $j "
-        ssh "$i" libego/twogtp/students/client.py &
-        sleep 1
+for c in $colors; do
+    for i in `seq -w 0 16`; do
+        echo "$c$i `ssh $c$i hostname 2> /dev/null`" &
+        sleep 0.2
     done
 done
+
+# for i in \
+#     khaki02 khaki03 khaki04 khaki05 khaki06 khaki07 khaki08 \
+#     khaki09 khaki10 khaki11 khaki12 khaki13 khaki14 khaki15 khaki16;
+# do
+#     for j in 1 2 3 4; do
+#         echo -n " $j "
+#         ssh "$i" libego/twogtp/students/client.py &
+#         sleep 1
+#     done
+# done
