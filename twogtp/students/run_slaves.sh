b5277fc2805163e145e882a5d40f746205393d0a
Lukasz Lew <lukasz.lew@gmail.com>
2010-03-23 17:10:39 +0100

    run_slaves speedup.

--- twogtp/students/run_slaves.sh
+++ twogtp/students/run_slaves.sh
@@ -6,7 +6,7 @@ for c in $colors; do
     for i in `seq -w 0 16`; do
         for k in `seq 4`; do
             echo "$c$i $k"
-            ssh "$c$i" screen -dm python libego/twogtp/students/client.py
+            ssh "$c$i" screen -dm python libego/twogtp/students/client.py &
             sleep 0.2
         done
     done
