656e9377d9369d0b00e64343819d3d15f7015f39
Lukasz Lew <lukasz.lew@gmail.com>
2010-03-23 13:51:59 +0100

    run_slaves.sh should work now

--- twogtp/students/run_slaves.sh
+++ twogtp/students/run_slaves.sh
@@ -4,18 +4,10 @@ colors="red pink orange brown yellow khaki cyan green violet blue crimson"
 
 for c in $colors; do
     for i in `seq -w 0 16`; do
-        echo "$c$i `ssh $c$i hostname 2> /dev/null`" &
-        sleep 0.2
+        for k in `seq 4`; do
+            echo "$c$i $k"
+            ssh "$c$i" screen -dm python libego/twogtp/students/client.py
+            sleep 0.2
+        done
     done
 done
-
-# for i in \
-#     khaki02 khaki03 khaki04 khaki05 khaki06 khaki07 khaki08 \
-#     khaki09 khaki10 khaki11 khaki12 khaki13 khaki14 khaki15 khaki16;
-# do
-#     for j in 1 2 3 4; do
-#         echo -n " $j "
-#         ssh "$i" libego/twogtp/students/client.py &
-#         sleep 1
-#     done
-# done
